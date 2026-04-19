17c80f0c6fcab4c4cdf909152dd07f5c2a1c65e8
AustinD123 <austinlm10dcosta@gmail.com>
2026-04-19 19:01:56 +0530

    minor fixees

--- docksmith/build/CMakeFiles/docksmith.dir/engine/layers.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/docksmith.dir/engine/layers.cpp.obj: \
- C:\Users\HP\projects\docksmith-cc\docksmith\engine\layers.cpp
--- docksmith/build/CMakeFiles/docksmith_integration_phases.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for docksmith_integration_phases.
-# This may be replaced when dependencies are built.
